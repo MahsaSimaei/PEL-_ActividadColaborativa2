a4f2c16db1a6f569eee0965801c314a2fe136329
MahsaSimaei <97670499+MahsaSimaei@users.noreply.github.com>
2025-10-18 23:34:50 +0200

    No virtual
    No virtual

--- Usuario.h
+++ Usuario.h
@@ -18,7 +18,7 @@ public:
     Usuario(string nombre, string apellido, string passwd, int id)
         : nombre(nombre), apellido(apellido), passwd(passwd), id(id), alta(true) {}
 
-    virtual ~Usuario() = default;
+    ~Usuario() = default;
 
     void darAlta() { alta = true; }
     void darBaja() { alta = false; }
@@ -32,7 +32,7 @@ public:
     void setNombre(string n) { nombre = n; }
     void setApellido(string a) { apellido = a; }
 
-    virtual string toString() const {
+    string toString() const {
         string estado = alta ? "Alta" : "Baja";
         return "ID: " + std::to_string(id) + " | " + nombre + " " + apellido + " (" + estado + ")";
     }
@@ -44,13 +44,11 @@ private:
 
 public:
     Estudiante() : Usuario() {
-        // Por defecto, usamos "mochila_default" como carpeta
         mochila = new MochilaDigital("mochila_default");
     }
 
     Estudiante(std::string nombre, std::string apellido, std::string passwd, int id)
         : Usuario(nombre, apellido, passwd, id) {
-        // Carpeta mochila personalizada por nombre de usuario
         mochila = new MochilaDigital("mochila_" + nombre);
     }
 
