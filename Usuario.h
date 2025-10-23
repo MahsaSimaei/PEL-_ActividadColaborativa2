0e6d64125ecad3c9b2ff7e3577466a7c134b9c13
AthinaMH <athinamh@gmail.com>
2025-10-23 18:59:03 +0200

    Calculadora y Usuario

--- Usuario.h
+++ Usuario.h
@@ -1,11 +1,12 @@
 #ifndef USUARIO_H
 #define USUARIO_H
-#include "MochilaDigital.h"
-#include <string>
-using std::string;
+#include <iostream>
+#include <ostream>
+#include <random>
+#include "Listas.h"
 
 class Usuario {
-protected:
+private:
     std::string nombre;
     std::string apellido;
     std::string passwd;
@@ -13,50 +14,160 @@ protected:
     bool alta;
 
 public:
-    Usuario() : nombre(""), apellido(""), passwd(""), id(0), alta(false) {}
-
-    Usuario(std::string nombre, std::string apellido, std::string passwd, int id)
-        : nombre(nombre), apellido(apellido), passwd(passwd), id(id), alta(true) {}
-
-    ~Usuario() = default;
-
-    void darAlta() { alta = true; }
-    void darBaja() { alta = false; }
-
-    std::string getNombreCompleto() const { return nombre + " " + apellido; }
-    std::string getNombre() const { return nombre; }
-    std::string getPasswd() const { return passwd; }
-    int getId() const { return id; }
-    bool isAlta() const { return alta; }
-
-    void setNombre(std::string n) { nombre = n; }
-    void setApellido(std::string a) { apellido = a; }
-
-    std::string toString() const {
-        std::string estado = alta ? "Alta" : "Baja";
-        return "ID: " + std::to_string(id) + " | " + nombre + " " + apellido + " (" + estado + ")";
+    Usuario() {
+        nombre = "";
+        apellido="";
+        passwd = "";
+        id = 0;
+        alta = false;
     }
+
+    Usuario(std::string nombre,std::string apellido, std::string pwd, int id) {
+        this->nombre = nombre;
+        this->apellido = apellido;
+        this->passwd = passwd;
+        this->id = id;
+        alta = false;
+    }
+
+    void darAlta() {
+        alta = true;
+    }
+
+    std::string getNombre() {
+        return nombre + " " + apellido;
+    }
+
+    std::string getUsuario() {
+        return nombre;
+    }
+
+    std::string getPasswd() {
+        return passwd;
+    }
+
+    bool isAlta() {
+        return alta;
+    }
+
+    int getId() {
+        return id;
+    }
+
+    void setNombre(std::string nombre) {
+        this->nombre = nombre;
+    }
+
+    void setApellido(std::string apellido) {
+        this->apellido = apellido;
+    }
+
+    std::string toString() {
+        std::string ret;
+        ret = "Nombre: " + nombre + " " + apellido + ", ID: " + std::to_string(id);
+        ret += alta ? ", Esta de alta." : ", Esta de baja";
+        return ret;
+    }
+
 };
 
 class Estudiante : public Usuario {
 private:
-    MochilaDigital* mochila; // En el heap
-
+    ListaDeObjetos<FILE> *mochila;
+    ListaDeObjetos<double> *calificaciones;
+    std::string clase;
 public:
-    Estudiante() : Usuario() {
-        mochila = new MochilaDigital("mochila_default");
+    Estudiante() {}
+
+    Estudiante(std::string nombre, std::string apellido, std::string pwd, int id) : Usuario(nombre,apellido,pwd,id) {
+        mochila = new ListaDeObjetos<FILE>();
+        calificaciones = new ListaDeObjetos<double>();
+        clase = "";
     }
 
-    Estudiante(std::string nombre, std::string apellido, std::string passwd, int id)
-        : Usuario(nombre, apellido, passwd, id) {
-        mochila = new MochilaDigital("mochila_" + nombre);
+    void setClase(std::string clase) {
+        this->clase = clase;
     }
 
-    ~Estudiante() {
-        delete mochila;
+    std::string getClase() {
+        return clase;
     }
 
-    MochilaDigital* getMochila() const { return mochila; }
+    void agregarCalificacion(double nota) {
+        calificaciones->append(nota);
+    }
+
+    double getCalificacion(int pos) {
+        return calificaciones->get(pos);
+    }
+
+    void eliminarCalificacion(int pos) {
+        calificaciones->erase(pos);
+    }
+
+    void agregarDoc(FILE doc) {
+        mochila->append(doc);
+    }
+
+    FILE getDoc(int pos) {
+        return mochila->get(pos);
+    }
+
+    void eliminarDoc(int pos) {
+        calificaciones->erase(pos);
+    }
+
+
+    std::string toString() {
+        return this->getNombre() + "\nNotas:\n" + calificaciones->toStringNoObj();
+    }
 };
 
-#endif
+class Profesor : public Usuario{
+private:
+    ListaDeObjetos<Estudiante> *clase;
+    std::string asignatura;
+public:
+    Profesor(){}
+
+    Profesor(std::string nombre, std::string apellido, std::string pwd, int id) : Usuario(nombre,apellido,pwd,id) {
+        clase = new ListaDeObjetos<Estudiante>();
+        asignatura = "";
+    }
+
+    void setAsignatura(std::string asignatura) {
+        this->asignatura = asignatura;
+    }
+
+    std::string getAsignatura() {
+        return asignatura;
+    }
+
+    void agregarEstudiante(Estudiante es) {
+        clase->append(es);
+    }
+
+    Estudiante getEstudiante(int pos) {
+        return clase->get(pos);
+    }
+
+    void eliminarEstudiante(int pos) {
+        clase->erase(pos);
+    }
+
+    std::string toString() {
+        return "Profesor: " + this->getNombre() + "\nClase: " + asignatura + "\nEstudiantes:\n" + clase->toString();
+    }
+};
+
+class Admin : public Usuario{
+public:
+    Admin(){}
+
+    Admin(std::string nombre, std::string apellido, std::string passwd, int id) : Usuario(nombre,apellido,passwd,id) {}
+
+};
+
+
+
+#endif //USUARIO_H
